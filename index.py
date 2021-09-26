93767d6d372b3485ec73943d7c61d018f300d20a
VittorioCoseglia <86205901+VittorioCoseglia@users.noreply.github.com>
2021-09-25 21:21:20 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+# Routes to Render Something
+
+
+@app.route('/')
+def phs():
+    return render_template("home.html")
+
+
+@app.route('/', methods=['POST'])
+def phsp():
+    text = request.form['text']
+    print(text)
+    return render_template(
+		'home.html',
+		info=text  
+	)
+
+
+
+if __name__ == '__main__':
+	app.run( # Starts the site
+		host='0.0.0.0',  # Establishes the host
+	)
